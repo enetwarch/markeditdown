92086aef5ef8df46ad0b05059256ee362b4c5b51
Enetwarch <entuxell@protonmail.com>
2026-03-29 19:13:04 +0800

    refactor: modularized repetitive code and elements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,57 @@
 <script lang="ts">
   import './layout.css';
   import favicon from '$lib/assets/favicon.svg';
+  import Icon from '@iconify/svelte';
 
   let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="flex min-h-screen flex-col gap-0">
+  <header class="flex w-full items-center justify-between border-b border-border bg-background p-4">
+    <nav class="flex items-center gap-12">
+      <a href="/">
+        <h1 class="font-space-grotesk text-3xl font-extrabold tracking-widest text-primary">
+          MARKEDITDOWN
+        </h1>
+      </a>
+      <ul class="flex gap-8">
+        {#snippet link(href: string, text: string)}
+          <li>
+            <a class="font-space-grotesk text-base tracking-wider text-foreground" {href}>
+              {text}
+            </a>
+          </li>
+        {/snippet}
+        {@render link('/editor', 'EDITOR')}
+        {@render link('/docs', 'DOCS')}
+      </ul>
+    </nav>
+    <nav>
+      <ul class="flex gap-4">
+        {#snippet link(href: string, icon: string)}
+          <li>
+            <a class="flex items-center justify-center" {href} target="_blank">
+              <Icon class="text-3xl text-subforeground" {icon}></Icon>
+            </a>
+          </li>
+        {/snippet}
+        {@render link(
+          'https://github.com/enetwarch/markeditdown',
+          'material-symbols:code-blocks-outline-sharp'
+        )}
+        {@render link(
+          'https://github.com/enetwarch',
+          'material-symbols:credit-card-heart-outline-sharp'
+        )}
+      </ul>
+    </nav>
+  </header>
+  {@render children()}
+  <footer class="flex w-full justify-center border-t border-border bg-background p-2">
+    <p class="font-space-grotesk text-subforeground">
+      Made with
+      <a class="underline" href="https://svelte.dev/" target="_blank">Svelte</a>
+    </p>
+  </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,106 +1,107 @@
 <script lang="ts">
   import Icon from '@iconify/svelte';
+  import { cn } from '$lib/utils';
+  import type { MouseEventHandler } from 'svelte/elements';
+
+  type File = {
+    type: 'file';
+    name: string;
+    size?: number;
+  };
+
+  type Directory = {
+    type: 'directory';
+    name: string;
+    children: (File | Directory)[];
+  };
+
+  let nodes: (File | Directory)[] = [
+    {
+      type: 'directory',
+      name: 'important',
+      children: [
+        { type: 'file', name: 'information' },
+        { type: 'file', name: 'data' }
+      ]
+    },
+    { type: 'file', name: 'readme' },
+    { type: 'file', name: 'sample' }
+  ];
 </script>
 
-<div class="flex min-h-screen flex-col gap-0">
-  <header
-    class="flex w-full items-center justify-between border-b border-border bg-background p-4"
+<div class="flex grow">
+  <aside
+    class="flex min-w-70 grow flex-col gap-4 overflow-y-auto border-r border-border bg-surface"
   >
-    <nav>
-      <a href="./">
-        <h1 class="font-space-grotesk text-3xl font-extrabold text-primary">MARKEDITDOWN</h1>
-      </a>
-    </nav>
-    <nav class="flex">
-      <ul class="flex gap-4">
-        <li class="flex items-center justify-center">
-          <a href="https://github.com/enetwarch/markeditdown" target="_blank">
-            <Icon
-              class="text-3xl text-subforeground"
-              icon="material-symbols:code-blocks-outline-sharp"
-            />
-          </a>
-        </li>
-        <li class="flex items-center justify-center">
-          <a href="https://github.com/enetwarch" target="_blank">
-            <Icon
-              class="text-3xl text-subforeground"
-              icon="material-symbols:credit-card-heart-outline-sharp"
-            />
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <div class="flex grow">
-    <aside class="flex min-w-70 flex-col gap-4 border-r border-border bg-surface">
-      <div class="w-full flex p-4 gap-4 space-between">
-        <header class="w-full flex flex-col">
-          <div class="flex gap-2">
-                    <h2 class="font-space-grotesk font-bold text-primary text-lg tracking-widest">COMPUTER</h2>
-          <button class="flex items-center justify-center hover:cursor-pointer">
-            <Icon class="text-xl text-subforeground"
-              icon="material-symbols:edit-outline-sharp" />
-          </button>
-          <button class="flex items-center justify-center hover:cursor-pointer">
-            <Icon class="text-xl text-subforeground"
-              icon="material-symbols:delete-outline-sharp" />
-          </button>
+    <div class="space-between flex w-full gap-4 p-4">
+      <header class="flex w-full flex-col">
+        <div class="flex gap-2">
+          <h2 class="font-space-grotesk text-lg font-bold tracking-widest text-primary">
+            COMPUTER
+          </h2>
+          {#snippet button(
+            label: string,
+            icon: string,
+            onclick?: MouseEventHandler<HTMLButtonElement>
+          )}
+            <button
+              class="flex items-center justify-center hover:cursor-pointer"
+              aria-label={label}
+              {onclick}
+            >
+              <Icon class="text-xl text-subforeground" {icon} />
+            </button>
+          {/snippet}
+          {@render button('Rename Button', 'material-symbols:edit-outline-sharp')}
+          {@render button('Delete Button', 'material-symbols:delete-outline-sharp')}
         </div>
-          <p class="font-space-grotesk text-sm text-subforeground">Last edited 20m ago</p>
-        </header>
-      </div>
-      <ul class="flex w-full flex-col">
-        <li class="flex w-full flex-col">
-          <div class="flex items-center gap-2 px-4 py-2 pl-4 hover:bg-foreground">
-            <Icon
-              class="text-xl text-foreground"
-              icon="material-symbols:folder-open-outline-sharp"
-            />
-            <p class="font-space-grotesk text-base text-foreground">IMPORTANT</p>
-          </div>
-          <ul class="flex w-full flex-col">
-            <li class="flex w-full flex-col">
-              <div class="flex items-center gap-2 px-4 py-2 pl-8 hover:bg-foreground">
-                <Icon class="text-xl text-foreground" icon="material-symbols:docs-outline-sharp" />
-                <p class="font-space-grotesk text-base text-foreground">COMPUTER</p>
-              </div>
-            </li>
-            <li class="flex w-full flex-col">
-              <div class="flex items-center gap-2 px-4 py-2 pl-8 hover:bg-foreground">
-                <Icon class="text-xl text-foreground" icon="material-symbols:docs-outline-sharp" />
-                <p class="font-space-grotesk text-base text-foreground">PHONE</p>
-              </div>
-            </li>
-            <li class="flex w-full flex-col">
-              <div class="flex items-center gap-2 px-4 py-2 pl-8 hover:bg-foreground">
-                <Icon class="text-xl text-foreground" icon="material-symbols:docs-outline-sharp" />
-                <p class="font-space-grotesk text-base text-foreground">DATE</p>
-              </div>
-            </li>
-          </ul>
-        </li>
-        <li class="flex w-full flex-col">
-          <div class="flex items-center gap-2 px-4 py-2 pl-4 hover:bg-foreground">
-            <Icon class="text-xl text-foreground" icon="material-symbols:docs-outline-sharp" />
-            <p class="font-space-grotesk text-base text-foreground">DATA</p>
-          </div>
-        </li>
-      </ul>
-    </aside>
-    <main class="grow flex w-full flex-col gap-0">
-      <header class="flex w-full bg-surface border-b border-border">
-        <button class="flex items-center justify-center px-8 py-2 border-r border-border hover:cursor-pointer">
-          <h2 class="font-space-grotesk text-lg text-foreground">EDIT</h2>
-        </button>
-        <button class="flex items-center justify-center px-8 py-2 border-r border-border hover:cursor-pointer">
-          <h2 class="font-space-grotesk text-lg text-foreground">PREVIEW</h2>
-        </button>
+        <p class="font-space-grotesk text-sm text-subforeground">Last edited 20m ago</p>
       </header>
-      <div class="grow flex gap-0">
-        <section class="grow flex bg-background border-r border-border"></section>
-        <section class="grow flex bg-surface"></section>
-      </div>
-    </main>
-  </div>
+    </div>
+    <ul class="flex w-full flex-col">
+      {#snippet tree(nodes: (File | Directory)[], pl: number, increment: number)}
+        {#each nodes as node}
+          <li class="flex w-full flex-col">
+            <div class="flex items-center gap-2 py-2 pr-4" style:padding-left="{pl * 0.25}rem">
+              <Icon
+                class="text-xl text-foreground"
+                icon={node.type == 'file'
+                  ? 'material-symbols:docs-outline-sharp'
+                  : 'material-symbols:folder-outline-sharp'}
+              />
+              <span class="font-space-grotesk text-base tracking-wider text-foreground">
+                {node.name.toUpperCase()}
+              </span>
+            </div>
+            {#if node.type == 'directory'}
+              {@render tree(node.children, pl + increment, increment)}
+            {/if}
+          </li>
+        {/each}
+      {/snippet}
+      {@render tree(nodes, 4, 4)}
+    </ul>
+  </aside>
+  <main class="flex w-full grow flex-col gap-0">
+    <header class="flex w-full border-b border-border bg-surface">
+      {#snippet mode(text: string, onclick?: MouseEventHandler<HTMLButtonElement>)}
+        <button
+          class={cn(
+            'flex items-center justify-center border-x border-border px-8 py-2',
+            'font-space-grotesk text-base tracking-wider text-foreground hover:cursor-pointer'
+          )}
+          draggable="true"
+          {onclick}
+        >
+          {text.toUpperCase()}
+        </button>
+      {/snippet}
+      {@render mode('EDIT')}
+      {@render mode('PREVIEW')}
+    </header>
+    <div class="flex grow gap-0">
+      <section class="flex grow border-r border-border bg-background"></section>
+      <section class="flex grow bg-surface"></section>
+    </div>
+  </main>
 </div>
