3834e7ec6e57d59ef33b3d24b0c5e8dd91ccd264
Enetwarch <entuxell@protonmail.com>
2026-04-09 15:10:48 +0800

    feat: removed file manager sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,7 @@
 <script lang="ts">
-  import type { Node } from './types';
-  import Files from './Files.svelte';
   import Editor from './Editor.svelte';
-
-  let selectedNode: Node | null = $state(null);
-  let nodes: Node[] = $state([
-    {
-      id: crypto.randomUUID(),
-      type: 'directory',
-      name: 'important',
-      children: [
-        { id: crypto.randomUUID(), type: 'file', name: 'information' },
-        { id: crypto.randomUUID(), type: 'file', name: 'data' }
-      ]
-    },
-    { id: crypto.randomUUID(), type: 'file', name: 'readme' },
-    { id: crypto.randomUUID(), type: 'file', name: 'sample' }
-  ]);
 </script>
 
 <div class="flex grow">
-  <Files class="min-w-70 grow border-r border-border" {nodes} {selectedNode} />
   <Editor class="w-full grow" />
 </div>
