07dd052012e5191775a16f6b7a4b2eea925e31ae
Enetwarch <entuxell@protonmail.com>
2026-04-09 15:19:19 +0800

    transferred Editor.svelte content to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,96 @@
 <script lang="ts">
-  import Editor from './Editor.svelte';
+  import { cn } from '$lib/utils';
+  import type { HTMLButtonAttributes } from 'svelte/elements';
+
+  let editModeActive = $state(true);
+  let previewModeActive = $state(true);
+  let onlyEditModeActive = $derived(editModeActive && !previewModeActive);
+  let onlyPreviewModeActive = $derived(previewModeActive && !editModeActive);
+  let bothModesActive = $derived(editModeActive && previewModeActive);
+
+  let mainReference = $state<HTMLElement>();
+  let editSplitPercent = $state(50);
+  let isResizing = $state(false);
+
+  function handleResize(event: MouseEvent) {
+    if (!isResizing || !mainReference) return;
+    const mainRectangle = mainReference.getBoundingClientRect();
+    const relativeX = event.clientX - mainRectangle.left;
+    let newSplit = (relativeX / mainRectangle.width) * 100;
+    editSplitPercent = Math.min(Math.max(newSplit, 20), 80);
+  }
+
+  function startResize(event: MouseEvent) {
+    event.preventDefault(); // allows rapid resizing
+    isResizing = true;
+    window.addEventListener('mousemove', handleResize);
+    window.addEventListener('mouseup', stopResize);
+  }
+
+  function stopResize() {
+    isResizing = false;
+    window.removeEventListener('mousemove', handleResize);
+    window.removeEventListener('mouseup', stopResize);
+  }
 </script>
 
 <div class="flex grow">
-  <Editor class="w-full grow" />
+  <main bind:this={mainReference} class="flex w-full grow flex-col gap-0 bg-background">
+    <header class="relative flex w-full border-b border-border bg-surface">
+      {#snippet modeButton(text: string, { class: className, ...rest }: HTMLButtonAttributes = {})}
+        <button
+          class={cn(
+            'flex items-center justify-center border-x border-b-3 border-x-border border-b-transparent px-8 py-2 font-space-grotesk text-base tracking-wider text-foreground hover:cursor-pointer data-[active=true]:border-b-primary data-[active=true]:font-bold data-[active=true]:tracking-widest data-[active=true]:text-primary',
+            className
+          )}
+          {...rest}
+        >
+          {text.toUpperCase()}
+        </button>
+      {/snippet}
+      {@render modeButton('EDIT', {
+        class: 'border-l-0',
+        'data-active': editModeActive,
+        'data-onlyactive': onlyEditModeActive,
+        onclick: () => {
+          if (onlyEditModeActive) return;
+          editModeActive = !editModeActive;
+        }
+      })}
+      {@render modeButton('PREVIEW', {
+        class:
+          'data-[active=true]:absolute data-[active=false]:border-l-0 data-[onlyactive=true]:static data-[onlyactive=true]:border-l-0',
+        style: `left: ${editSplitPercent}%`,
+        'data-active': previewModeActive,
+        'data-onlyactive': onlyPreviewModeActive,
+        onclick: () => {
+          if (onlyPreviewModeActive) return;
+          previewModeActive = !previewModeActive;
+        }
+      })}
+    </header>
+    <div class="flex grow gap-0">
+      <section
+        class="flex grow bg-background data-[active=false]:hidden"
+        style="width: {editSplitPercent}%"
+        data-active={editModeActive}
+        data-onlyactive={onlyEditModeActive}
+        aria-label="Edit Section"
+      ></section>
+      <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+      <div
+        role="separator"
+        onmousedown={startResize}
+        data-active={bothModesActive}
+        class="z-999 -mx-2 h-full w-4 cursor-col-resize opacity-0 data-[active=false]:hidden"
+      ></div>
+      <section
+        class="flex grow resize bg-surface data-[active=false]:hidden data-[active=true]:border-l data-[active=true]:border-border data-[onlyactive=true]:border-l-0"
+        style="width: {100 - editSplitPercent}%"
+        data-active={previewModeActive}
+        data-onlyactive={onlyPreviewModeActive}
+        aria-label="Preview Section"
+      ></section>
+    </div>
+  </main>
 </div>
